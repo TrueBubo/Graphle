13bbd45fae5eafe55774359273da486c4085d1e1
Filip Bubák <bubak.filip@pm.me>
2025-05-11 14:40:54 +0200

    Start back-end project

---
+++ GraphleManager/src/main/java/com/graphle/graphlemanager/GraphleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.graphle.graphlemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphleManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphleManagerApplication.class, args);
+    }
+
+}
