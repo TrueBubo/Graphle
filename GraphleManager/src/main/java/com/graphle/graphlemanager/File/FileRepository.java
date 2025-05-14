ac10a005231335bd88c9269df86f86a9f6401eb6
Filip Bubák <bubak.filip@pm.me>
2025-05-14 21:41:36 +0200

    Add support for querying tags for files

---
+++ GraphleManager/src/main/java/com/graphle/graphlemanager/File/FileRepository.java
@@ -0,0 +1,16 @@
+package com.graphle.graphlemanager.File;
+
+import com.graphle.graphlemanager.Tag.Tag;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface FileRepository extends Neo4jRepository<File, UUID> {
+    @Query("MATCH (f:File {location: $fileLocation})-[:HasTag]->(t:Tag) RETURN t.id AS id, t.name AS name, t.value AS value, t.numericValue AS numericValue")
+    @NonNull List<Tag> tagsByFileLocation(@NonNull String fileLocation);
+}
--- GraphleManager/src/main/java/com/graphle/graphlemanager/Tag/Tag.java
+++ GraphleManager/src/main/java/com/graphle/graphlemanager/Tag/Tag.java
@@ -1,6 +1,14 @@
 package com.graphle.graphlemanager.Tag;
 
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
-public record Tag(@NonNull String name) {
+import java.util.UUID;
+
+public record Tag(@Id @GeneratedValue UUID id, @NonNull String name, @Nullable String value, @Nullable Double numericValue) {
+    public Tag(@NonNull String name, @Nullable String value, @Nullable Double numericValue) {
+        this(null, name, value, numericValue);
+    }
 }
