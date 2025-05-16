a31d81b2e782f06502bcbfb6b0b01414be472f58
Filip Bubák <bubak.filip@pm.me>
2025-05-16 19:27:24 +0200

    Remove dependency on Tag from Service to Controller

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileController.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileController.kt
@@ -1,6 +1,7 @@
 package com.graphle.graphlemanager.file
 
 import com.graphle.graphlemanager.connections.RangedNeighborConnections
+import com.graphle.graphlemanager.tag.TagService
 import com.graphle.graphlemanager.time.TimeRange
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -8,13 +9,13 @@ import org.springframework.stereotype.Controller
 import java.lang.System.currentTimeMillis
 
 @Controller
-class FileController(private val fileService: FileService) {
+class FileController(private val fileService: FileService, private val tagService: TagService) {
     @QueryMapping
     fun fileByLocation(@Argument location: String): File {
         return File(
             location,
             currentTimeMillis(),
-            fileService.tagsForFileLocation(location),
+            tagService.tagsForFileLocation(location),
             RangedNeighborConnections(
                 TimeRange(0, currentTimeMillis()),
                 listOf()
