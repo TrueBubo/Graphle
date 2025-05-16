cb045499dea08c0cc13e262d09249190dd801704
Filip Bubák <bubak.filip@pm.me>
2025-05-16 19:27:24 +0200

    Remove dependency on Tag from Service to Controller

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileService.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileService.kt
@@ -1,14 +1,6 @@
 package com.graphle.graphlemanager.file
 
-import com.graphle.graphlemanager.tag.Tag
-import com.graphle.graphlemanager.tag.TagRepository
-import org.springframework.lang.NonNull
 import org.springframework.stereotype.Service
 
 @Service
-class FileService(private val fileRepository: FileRepository, private val tagRepository: TagRepository) {
-    @NonNull
-    fun tagsForFileLocation(@NonNull location: String): List<Tag> {
-        return tagRepository.tagsByFileLocation(location)
-    }
-}
+class FileService(private val fileRepository: FileRepository)
