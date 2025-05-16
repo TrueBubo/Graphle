b6ee14ee40f920ab30be818ffda4188adc15c406
Filip Bubák <bubak.filip@pm.me>
2025-05-16 18:54:18 +0200

    Rename modules to adhere to naming conventions

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileRepository.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileRepository.kt
@@ -1,8 +1,9 @@
-package com.graphle.graphlemanager.Tag
+package com.graphle.graphlemanager.file
 
 import org.springframework.data.neo4j.repository.Neo4jRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface TagRepository : Neo4jRepository<Tag?, UUID?>
+interface FileRepository : Neo4jRepository<File, UUID> {
+}
--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/tag/Tag.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/tag/Tag.kt
@@ -1,21 +1,17 @@
-package com.graphle.graphlemanager.Tag
+package com.graphle.graphlemanager.tag
 
 import org.springframework.data.neo4j.core.schema.GeneratedValue
 import org.springframework.data.neo4j.core.schema.Id
-import org.springframework.lang.NonNull
-import org.springframework.lang.Nullable
 import java.util.*
 
 data class Tag(
     @GeneratedValue @Id val id: UUID?,
     val name: String,
     val value: String?,
-    val numericValue: Double?
 ) {
-    constructor(name: String, value: String?, numericValue: Double?) : this(
+    constructor(name: String, value: String?) : this(
         null,
         name,
-        value,
-        numericValue
+        value
     )
 }
