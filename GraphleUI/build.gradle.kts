fe0d03a2f7f12030f6a734805906289ad590d8da
Filip Bubák <bubak.filip@pm.me>
2025-05-16 15:07:37 +0200

    Add GraphleUI

---
+++ GraphleUI/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.apollographql.apollo") version "4.2.0"
+}
+
+group = "com.graphle"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+    implementation("com.apollographql.apollo:apollo-runtime:4.2.0")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.graphle")
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.graphle.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "GraphleUI"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ GraphleUI/src/main/kotlin/com/graphle/Tag.kt
@@ -0,0 +1,6 @@
+package com.graphle
+
+data class Tag(
+    val name: String,
+    val value: String?
+)
