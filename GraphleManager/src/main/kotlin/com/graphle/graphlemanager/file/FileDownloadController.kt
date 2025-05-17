e7cd52014e547029cd373834b070af7cffdf9e66
Filip Bubák <bubak.filip@pm.me>
2025-05-18 00:47:52 +0200

    Add downloading of files

---
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileDownloadController.kt
@@ -0,0 +1,40 @@
+package com.graphle.graphlemanager.file
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.RequestParam
+import java.net.URLConnection
+import java.nio.file.Files
+import java.nio.file.Paths
+
+@RestController
+class FileDownloadController {
+
+    @GetMapping("/download")
+    fun serveFile(@RequestParam("path") path: String): ResponseEntity<Resource> {
+        val filePath = Paths.get(path)
+        val resource: Resource = FileSystemResource(filePath)
+
+        if (!resource.exists()) return ResponseEntity.notFound().build()
+
+        val contentType = guessContentType(filePath.toString())
+        val fileName = filePath.fileName.toString()
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$fileName\"")
+            .header(HttpHeaders.CONTENT_TYPE, contentType)
+            .header(HttpHeaders.CONTENT_LENGTH, Files.size(filePath).toString())
+            .body(resource)
+    }
+
+
+    private fun guessContentType(filePath: String): String {
+        return URLConnection.guessContentTypeFromName(filePath) // From filename
+            ?: Files.probeContentType(Paths.get(filePath)) // From file header
+            ?: "application/octet-stream" // Fallback to binary
+    }
+}
