d6760c3038be364f09c9c8c8b27273e7d355e4f8
Filip Bubak <bubak.filip@pm.me>
2025-07-08 15:09:39 +0200

    Add adding tags via GraphQL

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/tag/TagController.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/tag/TagController.kt
@@ -1,6 +1,7 @@
 package com.graphle.graphlemanager.tag
 
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
@@ -9,4 +10,8 @@ class TagController(private val tagService: TagService) {
     @QueryMapping
     fun tagsByFileLocation(@Argument location: String): List<Tag> =
         tagService.tagsByFileLocation(location)
+
+    @MutationMapping
+    fun addTagToFile(@Argument location: String, @Argument tag: TagInput): Tag =
+        tagService.addTagToFile(location, tag)
 }
