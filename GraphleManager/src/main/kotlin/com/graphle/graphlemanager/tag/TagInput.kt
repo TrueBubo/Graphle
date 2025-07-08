7a65b4bb37366a928f5d934a758ff26ee676d3ab
Filip Bubak <bubak.filip@pm.me>
2025-07-08 15:09:39 +0200

    Add adding tags via GraphQL

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/tag/TagInput.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/tag/TagInput.kt
@@ -1,3 +1,19 @@
 package com.graphle.graphlemanager.tag
 
-data class TagInput()
+import com.graphle.graphlemanager.tag.Tag
+import org.springframework.data.neo4j.core.schema.GeneratedValue
+import org.springframework.data.neo4j.core.schema.Id
+import java.util.UUID
+
+data class TagInput(
+    @GeneratedValue @Id val id: UUID?,
+    val name: String,
+    val value: String?
+) {
+    constructor(name: String, value: String?) : this(
+        null,
+        name,
+        value
+    )
+}
+
