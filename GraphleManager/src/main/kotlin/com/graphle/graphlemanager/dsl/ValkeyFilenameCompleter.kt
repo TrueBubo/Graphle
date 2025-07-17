fabc2b8800fb9eed4417d305fdcd6d997d079c7d
Filip Bubák <bubak.filip@pm.me>
2025-07-18 00:24:41 +0200

    Fix TTL in insert

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/dsl/ValkeyFilenameCompleter.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/dsl/ValkeyFilenameCompleter.kt
@@ -3,8 +3,6 @@ package com.graphle.graphlemanager.dsl
 import io.valkey.Jedis
 import io.valkey.JedisPool
 import io.valkey.JedisPoolConfig
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.contract
 import kotlin.time.Duration.Companion.days
 
 inline fun <T> JedisPool.withJedis(action: (Jedis) -> T): T {
@@ -24,34 +22,24 @@ object ValkeyFilenameCompleter : FilenameCompleter {
     private val pool = JedisPool(poolConfig, HOST, PORT)
     private val ttl = 30.days
     private var lastElement = pool.withJedis { it.get("last") }?.toLong() ?: -1
-    private var root = pool.withJedis { it.get("0") }?.let { "0" }
+    private const val ROOT = "0"
 
-    @OptIn(ExperimentalContracts::class)
-    private fun initializeTrie(jedis: Jedis) {
-        jedis.set("last", "0")
-        root = "0"
-        jedis.hset(ValkeyFilenameCompleter.root, mapOf())
-        jedis.expire(ValkeyFilenameCompleter.root, ttl.inWholeSeconds)
 
-    }
 
     private fun insertComponent(jedis: Jedis, component: String, parent: String?): String {
-        initializeTrie(jedis)
+        if (lastElement == -1L) jedis.set("last", lastElement.toString())
 
-        var currNode = root!!
-        var currNodeChildren = jedis.hgetAll(currNode)
+        var currNode = ROOT
+        var currNodeChildren = jedis.hgetAll(currNode).toMutableMap()
         component.forEach {
-            if (jedis.get(currNode) == null) jedis.hset(currNode, mapOf())
-
-            jedis.expire(currNode, ttl.inWholeSeconds)
-
             val char = it.toString()
             val index = currNodeChildren[char] ?: (++lastElement).toString()
             currNodeChildren[char] = index
             jedis.hset(currNode, currNodeChildren)
+            jedis.expire(currNode, ttl.inWholeSeconds)
 
             currNode = index
-            currNodeChildren = jedis.hgetAll(currNode)
+            currNodeChildren = jedis.hgetAll(currNode).toMutableMap()
         }
 
         parent?.let { jedis.sadd("$currNode:parents", parent) }
