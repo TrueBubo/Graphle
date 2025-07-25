4c5fe2371fb04cdc1a5b4e43a7d60d5437e26f65
Filip Bubák <bubak.filip@pm.me>
2025-07-25 23:15:13 +0200

    Connect dsl completion with filenames

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/dsl/DSLAutoCompleter.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/dsl/DSLAutoCompleter.kt
@@ -1,13 +1,14 @@
 package com.graphle.graphlemanager.dsl
 
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class DSLAutoCompleter {
-    private fun completeFilename(filenamePrefix: String): List<String> {
-        TODO()
-    }
+    private fun completeFilename(filenamePrefix: String): List<String> =
+        ValkeyFilenameCompleter.lookup(filenamePrefix).map { it.joinToString(File.separator) }
+
 
     fun complete(commandPrefix: String): List<String> =
-        listOf(commandPrefix, "$commandPrefix + the rest")
+        listOf(commandPrefix, "$commandPrefix + the rest") + completeFilename(commandPrefix)
 }
