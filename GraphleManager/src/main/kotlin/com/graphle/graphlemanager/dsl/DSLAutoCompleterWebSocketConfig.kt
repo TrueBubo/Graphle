a3ed57b3d4d51f0572a01a3f95903ebe818787fb
Filip Bubák <bubak.filip@pm.me>
2025-07-28 22:35:34 +0200

    Add documentation comments for autocomplete websocket connections

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/dsl/DSLAutoCompleterWebSocketConfig.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/dsl/DSLAutoCompleterWebSocketConfig.kt
@@ -5,6 +5,10 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
+/**
+ * Configuration for autocomplete websockets
+ * @param dslAutoCompleter Autocomplete for handing requests
+ */
 @Configuration
 @EnableWebSocket
 open class DSLAutoCompleterWebSocketConfig(private val dslAutoCompleter: DSLAutoCompleter) : WebSocketConfigurer {
