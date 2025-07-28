b4df4332a9df303cdbb55e69b05106ae142bd91e
Filip Bubák <bubak.filip@pm.me>
2025-07-28 12:39:35 +0200

    Set theme based on OS theme

--- GraphleUI/src/main/kotlin/com/graphle/Main.kt
+++ GraphleUI/src/main/kotlin/com/graphle/Main.kt
@@ -1,10 +1,15 @@
 package com.graphle
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,8 +17,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
@@ -34,6 +39,25 @@ import kotlinx.serialization.json.Json
 import kotlinx.coroutines.launch
 import java.lang.System.currentTimeMillis
 import kotlin.time.Duration.Companion.milliseconds
+
+val DarkColorPalette = darkColors(
+    primary = Color(0xFFBB86FC),
+    background = Color(0xFF000000),   // true black
+    surface = Color(0xFF121212),      // optional: near-black for contrast
+    onPrimary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White
+)
+
+val LightColorPalette = lightColors(
+    primary = Color(0xFF6200EE),
+    background = Color.White,
+    surface = Color(0xFFEEEEEE),
+    onPrimary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
 
 const val serverURL = "http://localhost:8080/graphql"
 val minUpdateDelay = 1000.milliseconds
@@ -88,80 +112,93 @@ fun App() {
     var isLoading by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
 
-    MaterialTheme {
-        Column {
-            CommandLine()
+    MaterialTheme(colors = if (isSystemInDarkTheme()) DarkColorPalette else LightColorPalette) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column {
+                CommandLine()
 
-            TextField(
-                value = location,
-                onValueChange = { location = it},
-                singleLine = true,
-                modifier = Modifier.onPreviewKeyEvent { event ->
-                    val canRefresh = !isLoading && ((location != oldLocation)
-                            || (currentTimeMillis() - lastUpdated > minUpdateDelay.inWholeMilliseconds))
-                    if (event.key == Key.Enter && canRefresh) {
-                        oldLocation = location
-                        lastUpdated = currentTimeMillis()
-                        coroutineScope.launch {
-                            fetchFilesByLocation(
-                                location = location,
-                                onLoading = { isLoading = it },
-                                onResult = { file = it }
-                            )
-                        }
+                TextField(
+                    value = location,
+                    onValueChange = { location = it },
+                    singleLine = true,
+                    modifier = Modifier.onPreviewKeyEvent { event ->
+                        val canRefresh = !isLoading && ((location != oldLocation)
+                                || (currentTimeMillis() - lastUpdated > minUpdateDelay.inWholeMilliseconds))
+                        if (event.key == Key.Enter && canRefresh) {
+                            oldLocation = location
+                            lastUpdated = currentTimeMillis()
+                            coroutineScope.launch {
+                                fetchFilesByLocation(
+                                    location = location,
+                                    onLoading = { isLoading = it },
+                                    onResult = { file = it }
+                                )
+                            }
 
-                        true
+                            true
 
-                    } else false
+                        } else false
+                    }
+                )
+
+                TextField(
+                    value = tagName,
+                    onValueChange = { tagName = it },
+                    singleLine = true,
+                    modifier = Modifier.onPreviewKeyEvent { event ->
+                        if (event.key == Key.Enter && event.type == KeyEventType.KeyUp) {
+                            val tagNameSnapshot = tagName
+                            val tagValueSnapshot = tagValue
+                            coroutineScope.launch {
+                                if (tagNameSnapshot == "") return@launch
+                                if (tagValue != "") apolloClient.addTagToFile(
+                                    location,
+                                    tagNameSnapshot,
+                                    tagValueSnapshot
+                                )
+                                else apolloClient.addTagToFile(location, tagNameSnapshot)
+                            }
+
+                            tagName = ""
+                            tagValue = ""
+                            true
+                        } else false
+                    }
+                )
+
+                TextField(
+                    value = tagValue,
+                    onValueChange = { tagValue = it },
+                    singleLine = true,
+                    modifier = Modifier.onPreviewKeyEvent { event ->
+                        if (event.key == Key.Enter && event.type == KeyEventType.KeyUp) {
+                            val tagNameSnapshot = tagName
+                            val tagValueSnapshot = tagValue
+                            coroutineScope.launch {
+                                if (tagNameSnapshot == "") return@launch
+                                if (tagValueSnapshot != "") apolloClient.addTagToFile(
+                                    location,
+                                    tagNameSnapshot,
+                                    tagValueSnapshot
+                                )
+                                else apolloClient.addTagToFile(location, tagNameSnapshot)
+                            }
+
+                            tagName = ""
+                            tagValue = ""
+                            true
+                        } else false
+                    }
+                )
+
+                if (isLoading) {
+                    Text("Loading...")
+                } else {
+                    Text("File:\n$file")
                 }
-            )
-
-            TextField(
-                value = tagName,
-                onValueChange = { tagName = it },
-                singleLine = true,
-                modifier = Modifier.onPreviewKeyEvent { event ->
-                    if (event.key == Key.Enter && event.type == KeyEventType.KeyUp) {
-                        val tagNameSnapshot = tagName
-                        val tagValueSnapshot = tagValue
-                        coroutineScope.launch {
-                            if (tagNameSnapshot == "") return@launch
-                            if (tagValue != "") apolloClient.addTagToFile(location, tagNameSnapshot, tagValueSnapshot)
-                            else apolloClient.addTagToFile(location, tagNameSnapshot)
-                        }
-
-                        tagName = ""
-                        tagValue = ""
-                        true
-                    } else false
-                }
-            )
-
-            TextField(
-                value = tagValue,
-                onValueChange = { tagValue = it },
-                singleLine = true,
-                modifier = Modifier.onPreviewKeyEvent { event ->
-                    if (event.key == Key.Enter && event.type == KeyEventType.KeyUp) {
-                        val tagNameSnapshot = tagName
-                        val tagValueSnapshot = tagValue
-                        coroutineScope.launch {
-                            if (tagNameSnapshot == "") return@launch
-                            if (tagValueSnapshot != "") apolloClient.addTagToFile(location, tagNameSnapshot, tagValueSnapshot)
-                            else apolloClient.addTagToFile(location, tagNameSnapshot)
-                        }
-
-                        tagName = ""
-                        tagValue = ""
-                        true
-                    } else false
-                }
-            )
-
-            if (isLoading) {
-                Text("Loading...")
-            } else {
-                Text("File:\n$file")
             }
         }
     }
@@ -193,10 +230,17 @@ suspend fun ApolloClient.getFilesByLocation(location: String): ApolloResponse<Fi
     query(FileByLocationQuery(location)).execute()
 
 
-suspend fun ApolloClient.addTagToFile(location: String, name: String): ApolloResponse<AddTagToFileWithNameMutation.Data>  =
+suspend fun ApolloClient.addTagToFile(
+    location: String,
+    name: String
+): ApolloResponse<AddTagToFileWithNameMutation.Data> =
     mutation(AddTagToFileWithNameMutation(location, name)).execute()
 
-suspend fun ApolloClient.addTagToFile(location: String, name: String, value: String): ApolloResponse<AddTagToFileWithNameAndValueMutation.Data> =
+suspend fun ApolloClient.addTagToFile(
+    location: String,
+    name: String,
+    value: String
+): ApolloResponse<AddTagToFileWithNameAndValueMutation.Data> =
     mutation(AddTagToFileWithNameAndValueMutation(location, name, value)).execute()
 
 
