b0090a04ea8607f8fc45ce9011c8cf9c4ce39f21
Filip Bubák <bubak.filip@pm.me>
2025-08-07 12:10:09 +0200

    Implement sweeping non-existing files

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileRepository.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileRepository.kt
@@ -1,9 +1,12 @@
 package com.graphle.graphlemanager.file
 
 import org.springframework.data.neo4j.repository.Neo4jRepository
+import org.springframework.data.neo4j.repository.query.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface FileRepository : Neo4jRepository<File, UUID> {
+    @Query("MATCH (n:File {location: \$fileLocation})-[r]-(m) delete n,r")
+    fun removeFileByLocation(fileLocation: String)
 }
--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileService.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileService.kt
@@ -3,4 +3,8 @@ package com.graphle.graphlemanager.file
 import org.springframework.stereotype.Service
 
 @Service
-class FileService(private val fileRepository: FileRepository)
+class FileService(private val fileRepository: FileRepository, private val fileSweeper: FileSweeper) {
+    init {
+        fileSweeper.startSweeping()
+    }
+}
