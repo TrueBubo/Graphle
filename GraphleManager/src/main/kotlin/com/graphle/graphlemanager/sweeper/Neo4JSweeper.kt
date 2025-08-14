39434bdb38bf335ede126eedfb8b76cf55b56e2c
Filip Bubak <bubak.filip@pm.me>
2025-08-15 00:33:10 +0200

    Add Tests for Neo4J sweeper testing

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/sweeper/Neo4JSweeper.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/sweeper/Neo4JSweeper.kt
@@ -1,16 +1,19 @@
 package com.graphle.graphlemanager.sweeper
 
+import com.graphle.graphlemanager.file.AbsolutePathString
 import com.graphle.graphlemanager.file.File
 import com.graphle.graphlemanager.file.FileRepository
 import com.graphle.graphlemanager.tag.TagRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.springframework.stereotype.Service
 import java.nio.file.Files
 import kotlin.io.path.Path
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 /**
@@ -20,26 +23,32 @@ import kotlin.time.Duration.Companion.minutes
  */
 @Service
 class Neo4JSweeper(private val fileRepository: FileRepository, private val tagRepository: TagRepository) {
+    private var _sweepInterval = 1.minutes
+    fun setSweepInterval(value: Duration) {
+            scope.cancel()
+            _sweepInterval = value
+            startSweeping()
+        }
+
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    private val sweepInterval = 1.minutes
 
     init {
-        scope.launch { startSweeping() }
+        startSweeping()
     }
 
-    private fun sweep() {
+    fun sweep(filesExistsPredicate: (AbsolutePathString) -> Boolean = { path -> Files.exists(Path(path))}) {
         fileRepository
             .findAll()
             .map(File::location)
-            .filter { !Files.exists(Path(it)) }
+            .filter { !filesExistsPredicate(it) }
             .forEach { fileRepository.removeFileByLocation(it) }
         tagRepository.removeOrphanTags()
     }
 
-    private fun startSweeping() {
+    private fun startSweeping(filesExistsPredicate: (AbsolutePathString) -> Boolean = { path -> Files.exists(Path(path)) }) {
         scope.launch {
             while (true) {
-                delay(sweepInterval)
+                delay(_sweepInterval)
                 sweep()
             }
         }
