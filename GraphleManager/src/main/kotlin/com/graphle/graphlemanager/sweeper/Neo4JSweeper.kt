23ad20cb6ec0e223ac83c41576fd5f049862935a
Filip Bubak <bubak.filip@pm.me>
2025-08-15 13:20:13 +0200

    Enable setting properties in spring

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/sweeper/Neo4JSweeper.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/sweeper/Neo4JSweeper.kt
@@ -7,14 +7,15 @@ import com.graphle.graphlemanager.tag.TagRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.nio.file.Files
 import kotlin.io.path.Path
-import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toJavaDuration
+import kotlin.time.toKotlinDuration
 
 /**
  * Used to clean the system of nodes which do not exist
@@ -22,8 +23,11 @@ import kotlin.time.Duration.Companion.minutes
  * @param fileRepository Repository used for cleaning the database
  */
 @Service
-class Neo4JSweeper(private val fileRepository: FileRepository, private val tagRepository: TagRepository) {
-    private var _sweepInterval = 1.minutes
+class Neo4JSweeper(
+    private val fileRepository: FileRepository,
+    private val tagRepository: TagRepository,
+    @Value("\${neo4j.sweeper.interval}") private val sweepInterval: java.time.Duration = 1.minutes.toJavaDuration()
+) {
 
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
@@ -35,7 +39,7 @@ class Neo4JSweeper(private val fileRepository: FileRepository, private val tagRe
      * Forces sweeping to be done right now
      * @param filesExistsPredicate Checks whether the file is eligible to be swept
      */
-    fun sweep(filesExistsPredicate: (AbsolutePathString) -> Boolean = { path -> Files.exists(Path(path))}) {
+    fun sweep(filesExistsPredicate: (AbsolutePathString) -> Boolean = { path -> Files.exists(Path(path)) }) {
         fileRepository
             .findAll()
             .map(File::location)
@@ -52,7 +56,7 @@ class Neo4JSweeper(private val fileRepository: FileRepository, private val tagRe
     private fun startSweeping(filesExistsPredicate: (AbsolutePathString) -> Boolean = { path -> Files.exists(Path(path)) }) {
         scope.launch {
             while (true) {
-                delay(_sweepInterval)
+                delay(sweepInterval.toKotlinDuration())
                 sweep(filesExistsPredicate)
             }
         }
