e9f7a244b219af48b3db492fd12b0a6ac5448c78
Filip Bubak <bubak.filip@pm.me>
2025-08-21 19:44:45 +0200

    Make file manipulation mockable by passing actions as arguments

--- GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileService.kt
+++ GraphleManager/src/main/kotlin/com/graphle/graphlemanager/file/FileService.kt
@@ -7,19 +7,34 @@ import kotlin.io.path.Path
 
 @Service
 class FileService(private val fileRepository: FileRepository, private val fileSweeper: Neo4JSweeper) {
-    fun filesFromFileByRelationship(fromLocation: AbsolutePathString, relationshipName: String): List<AbsolutePathString> =
+    fun filesFromFileByRelationship(
+        fromLocation: AbsolutePathString,
+        relationshipName: String
+    ): List<AbsolutePathString> =
         fileRepository.getFileLocationsByConnections(fromLocation, relationshipName)
 
-    fun addFile(location: AbsolutePathString) {
-        Files.createFile(Path(location))
+    fun addFile(
+        location: AbsolutePathString,
+        createFileAction: (AbsolutePathString) -> Unit = { Files.createFile(Path(it)) }
+    ) {
+        createFileAction(location)
     }
 
-    fun removeFile(location: AbsolutePathString) {
-        Files.deleteIfExists(Path(location))
+    fun removeFile(
+        location: AbsolutePathString,
+        removeFileByLocationAction: (AbsolutePathString) -> Unit = { Files.deleteIfExists(Path(it)) }
+    ) {
+        removeFileByLocationAction(location)
         fileRepository.removeFileByLocation(location)
     }
 
-    fun moveFile(locationFrom: AbsolutePathString, locationTo: AbsolutePathString) {
-        Files.move(Path(locationFrom), Path(locationTo))
+    fun moveFile(
+        locationFrom: AbsolutePathString,
+        locationTo: AbsolutePathString,
+        moveFileAction: (AbsolutePathString, AbsolutePathString) -> Unit = { from, to ->
+            Files.move(Path(from), Path(to))
+        }
+    ) {
+        moveFileAction(locationFrom, locationTo)
     }
 }
