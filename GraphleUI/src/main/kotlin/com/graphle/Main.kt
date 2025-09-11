8e0a59ea189523c642c3e2eba4d9f8796368885e
Filip Bubák <bubak.filip@pm.me>
2025-09-12 00:11:36 +0200

    Enable going through the file graph

--- GraphleUI/src/main/kotlin/com/graphle/Main.kt
+++ GraphleUI/src/main/kotlin/com/graphle/Main.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.onClick
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -19,14 +21,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
-import androidx.compose.ui.input.key.type
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.ApolloResponse
 import kotlinx.coroutines.launch
 import java.lang.System.currentTimeMillis
 import kotlin.time.Duration.Companion.milliseconds
@@ -89,54 +89,26 @@ fun App() {
                     }
                 )
 
-                TextField(
+                TagTextField(
                     value = tagName,
                     onValueChange = { tagName = it },
-                    singleLine = true,
-                    modifier = Modifier.onPreviewKeyEvent { event ->
-                        if (event.key == Key.Enter && event.type == KeyEventType.KeyUp) {
-                            val tagNameSnapshot = tagName
-                            val tagValueSnapshot = tagValue
-                            coroutineScope.launch {
-                                if (tagNameSnapshot == "") return@launch
-                                if (tagValue != "") apolloClient.addTagToFile(
-                                    location,
-                                    tagNameSnapshot,
-                                    tagValueSnapshot
-                                )
-                                else apolloClient.addTagToFile(location, tagNameSnapshot)
-                            }
-
-                            tagName = ""
-                            tagValue = ""
-                            true
-                        } else false
-                    }
+                    location = location,
+                    tagName = tagName,
+                    tagValue = tagValue,
+                    tagNameSetter = { tagName = it },
+                    tagValueSetter = { tagValue = it },
+                    coroutineScope = coroutineScope,
                 )
 
-                TextField(
+                TagTextField(
                     value = tagValue,
                     onValueChange = { tagValue = it },
-                    singleLine = true,
-                    modifier = Modifier.onPreviewKeyEvent { event ->
-                        if (event.key == Key.Enter && event.type == KeyEventType.KeyUp) {
-                            val tagNameSnapshot = tagName
-                            val tagValueSnapshot = tagValue
-                            coroutineScope.launch {
-                                if (tagNameSnapshot == "") return@launch
-                                if (tagValueSnapshot != "") apolloClient.addTagToFile(
-                                    location,
-                                    tagNameSnapshot,
-                                    tagValueSnapshot
-                                )
-                                else apolloClient.addTagToFile(location, tagNameSnapshot)
-                            }
-
-                            tagName = ""
-                            tagValue = ""
-                            true
-                        } else false
-                    }
+                    location = location,
+                    tagName = tagName,
+                    tagValue = tagValue,
+                    tagNameSetter = { tagName = it },
+                    tagValueSetter = { tagValue = it },
+                    coroutineScope = coroutineScope,
                 )
 
                 if (isLoading) {
@@ -146,29 +118,37 @@ fun App() {
                     if (displayedInfo == null) Text("Could not find the file")
                     else {
                         displayedInfo?.get(PropertyType.FILE)
-                            ?.apply { Text("Files") }
-                            ?.forEach { filename ->
-                                Text(
-                                    text = filename,
-                                    modifier = Modifier.onClick(
-                                        onClick = {
-                                            println("Clicked $filename")
-                                            coroutineScope.launch {
-                                                fetchFilesByLocation(
-                                                    location = filename,
-                                                    onLoading = { isLoading = it },
-                                                    onResult = {
-                                                        location = filename
-                                                        displayedInfo = it
-                                                    }
-                                                )
-                                            }
-                                        }
-                                    )
-                                )
+                            ?.apply { Text(text = "Files", fontWeight = FontWeight.Bold) }
+                            ?.let { filenames ->
+                                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                                    items(items = filenames, key = { it }) { filename ->
+                                        FileBox(
+                                            filename = filename,
+                                            onLoading = { isLoading = it },
+                                            onResult = {
+                                                location = filename
+                                                displayedInfo = it
+                                            },
+                                            onRefresh = {
+                                                coroutineScope.launch {
+                                                    fetchFilesFromFileByRelationship(
+                                                        fromLocation = location,
+                                                        relationshipName = "descendant",
+                                                        onLoading = { isLoading = it },
+                                                        onResult = {
+                                                            displayedInfo =
+                                                                mapOf(PropertyType.FILE to (it ?: emptyList()))
+                                                        }
+                                                    )
+                                                }
+                                            },
+                                            coroutineScope = coroutineScope
+                                        )
+                                    }
+                                }
                             }
                         displayedInfo?.get(PropertyType.TAG)
-                            ?.apply { Text("Tags") }
+                            ?.apply { Text(text = "Tags", fontWeight = FontWeight.Bold) }
                             ?.forEach {
                                 Text(
                                     text = it,
@@ -177,23 +157,25 @@ fun App() {
                             }
 
                         displayedInfo?.get(PropertyType.CONNECTION)
-                            ?.apply { Text("Connections") }
+                            ?.apply { Text(text = "Connections", fontWeight = FontWeight.Bold) }
                             ?.forEach { relationshipName ->
-                                Text(
-                                    text = relationshipName,
-                                    modifier = Modifier.onClick(onClick = {
-                                        println("Clicked $relationshipName")
+                                RelationshipBox(
+                                    relationshipName = relationshipName,
+                                    location = location,
+                                    onLoading = { isLoading = it },
+                                    onResult = {
+                                        displayedInfo = mapOf(PropertyType.FILE to (it ?: emptyList()))
+                                    },
+                                    onRefresh = {
                                         coroutineScope.launch {
-                                            fetchFilesFromFileByRelationship(
-                                                fromLocation = location,
-                                                relationshipName = relationshipName,
+                                            fetchFilesByLocation(
+                                                location = location,
                                                 onLoading = { isLoading = it },
-                                                onResult = {
-                                                    displayedInfo = mapOf(PropertyType.FILE to (it ?: emptyList()))
-                                                }
+                                                onResult = { displayedInfo = it }
                                             )
                                         }
-                                    })
+                                    },
+                                    coroutineScope = coroutineScope
                                 )
                             }
                     }
@@ -208,67 +190,6 @@ fun App() {
         }
     }
 }
-
-suspend fun fetchFilesFromFileByRelationship(
-    fromLocation: String,
-    relationshipName: String,
-    onLoading: (Boolean) -> Unit,
-    onResult: (List<String>?) -> Unit
-) {
-    onLoading(true)
-    println("Ran relation")
-    val response = apolloClient.getFilesFromFileByRelationship(
-        fromLocation = fromLocation,
-        relationshipName = relationshipName
-    )
-    onResult(if (response.hasErrors()) null else response.data?.filesFromFileByRelationship)
-    onLoading(false)
-}
-
-suspend fun ApolloClient.getFilesFromFileByRelationship(
-    fromLocation: String,
-    relationshipName: String
-): ApolloResponse<FilesFromFileByRelationshipQuery.Data> =
-    query(FilesFromFileByRelationshipQuery(fromLocation, relationshipName)).execute()
-
-suspend fun fetchFilesByLocation(
-    location: String,
-    onLoading: (Boolean) -> Unit,
-    onResult: (Map<PropertyType, List<String>>?) -> Unit
-) {
-    onLoading(true)
-    val response = apolloClient.getFilesByLocation(location)
-    onResult(
-        if (response.hasErrors()) {
-            null
-        } else {
-            val file = response.data?.fileByLocation
-            if (file != null) mapOf(
-                PropertyType.TAG to file.tags.map { "${it.name}: ${it.value ?: ""}" },
-                PropertyType.CONNECTION to file.connections.map { it.relationship }
-            ) else null
-        }
-    )
-    onLoading(false)
-}
-
-suspend fun ApolloClient.getFilesByLocation(location: String): ApolloResponse<FileByLocationQuery.Data> =
-    query(FileByLocationQuery(location)).execute()
-
-
-suspend fun ApolloClient.addTagToFile(
-    location: String,
-    name: String
-): ApolloResponse<AddTagToFileWithNameMutation.Data> =
-    mutation(AddTagToFileWithNameMutation(location, name)).execute()
-
-suspend fun ApolloClient.addTagToFile(
-    location: String,
-    name: String,
-    value: String
-): ApolloResponse<AddTagToFileWithNameAndValueMutation.Data> =
-    mutation(AddTagToFileWithNameAndValueMutation(location, name, value)).execute()
-
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
---
+++ GraphleUI/src/main/kotlin/com/graphle/RelationshipBox.kt
@@ -0,0 +1,74 @@
+package com.graphle
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.onClick
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RelationshipBox(
+    relationshipName: String,
+    location: String,
+    onLoading: (Boolean) -> Unit,
+    onResult: (List<String>?) -> Unit,
+    onRefresh: () -> Unit,
+    coroutineScope: CoroutineScope
+) {
+    var showMenu by remember { mutableStateOf(false) }
+    Box {
+        Text(
+            text = relationshipName,
+            modifier = Modifier.onClick(onClick = {
+                println("Clicked $relationshipName")
+                coroutineScope.launch {
+                    fetchFilesFromFileByRelationship(
+                        fromLocation = location,
+                        relationshipName = relationshipName,
+                        onLoading = onLoading,
+                        onResult = onResult
+                    )
+                }
+            }).onClick(
+                matcher = PointerMatcher.mouse(PointerButton.Secondary),
+                onClick = {
+                    println("Right Clicked $relationshipName")
+                    showMenu = true
+                }
+            )
+        )
+        DropdownMenu(
+            expanded = showMenu,
+            onDismissRequest = { showMenu = false },
+        ) {
+            DropdownMenuItem(
+                content = { Text("Open") },
+                onClick = {
+                    showMenu = false
+                },
+            )
+            DropdownMenuItem(
+                content = { Text("Delete") },
+                onClick = {
+                    coroutineScope.launch {
+                        apolloClient.removeFileByLocation(location)
+                        onRefresh()
+                    }
+                    showMenu = false
+                }
+            )
+        }
+    }
+}
