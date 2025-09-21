d5c3df39a1db2808f82bd642cb41034debfae00d
Filip Bubak <bubak.filip@pm.me>
2025-09-21 20:08:48 +0200

    Refactor app layout into multiple functions

--- GraphleUI/settings.gradle.kts
+++ GraphleUI/settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm").version(extra["kotlin.version"] as String)
+        kotlin("jvm") version "2.1.21"
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
         id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
     }
