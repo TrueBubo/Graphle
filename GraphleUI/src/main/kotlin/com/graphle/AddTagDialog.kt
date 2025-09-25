80cfdaf94d901e78e1fb0cffbcfbb847d322be05
Filip Bubák <bubak.filip@pm.me>
2025-09-26 00:17:37 +0200

    Require name for add tag

--- GraphleUI/src/main/kotlin/com/graphle/AddTagDialog.kt
+++ GraphleUI/src/main/kotlin/com/graphle/AddTagDialog.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,12 +14,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 object AddTagDialog {
     private var location by mutableStateOf("")
     private var isShown by mutableStateOf(false)
+    private var showError by mutableStateOf(false)
 
     fun set(
         location: String,
@@ -39,10 +42,20 @@ object AddTagDialog {
             title = { Text("Enter information about the tag") },
             text = {
                 Column {
+                    if (showError) {
+                        Text(
+                            text = "Name field is required",
+                            color = Color.Red,
+                        )
+                    }
                     OutlinedTextField(
                         value = name,
-                        onValueChange = { name = it },
-                        label = { Text("Tag name") },
+                        onValueChange = {
+                            name = it
+                            showError = name.isBlank()
+                        },
+                        isError = showError,
+                        label = { Text("Tag name*") },
                         singleLine = true
                     )
                     Spacer(modifier = Modifier.height(8.dp))
@@ -63,8 +76,8 @@ object AddTagDialog {
                             Tag(name = name, value = value.ifBlank { null }).save(location = location)
                             onSubmitted()
                         }
-
-                    }
+                    },
+                    enabled = name.isNotBlank()
                 ) {
                     Text("OK")
                 }
