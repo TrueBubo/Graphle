08a8a46ddafe2278f5ab94e6a78ed579cd7f2b1b
Filip Bubák <bubak.filip@pm.me>
2025-09-26 00:58:35 +0200

    Require fields for add file

--- GraphleUI/src/main/kotlin/com/graphle/AddFileDialog.kt
+++ GraphleUI/src/main/kotlin/com/graphle/AddFileDialog.kt
@@ -10,6 +10,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import kotlinx.coroutines.launch
 
 object AddFileDialog {
@@ -25,14 +28,36 @@ object AddFileDialog {
     operator fun invoke(onConfirmed: suspend () -> Unit) {
         if (!isShown) return
         var filename by remember { mutableStateOf("") }
+
+        var showFileNameMissingError by mutableStateOf(false)
+        var hasInteractedWithFileName by mutableStateOf(false)
+
         AlertDialog(
             onDismissRequest = { isShown = false },
             title = { Text("Add new file") },
             text = {
                 Column {
+                    if (hasInteractedWithFileName && showFileNameMissingError) {
+                        Text(
+                            text = "Name field is required",
+                            color = Color.Red,
+                        )
+                    }
                     OutlinedTextField(
                         value = filename,
-                        onValueChange = { filename = it },
+                        onValueChange = {
+                            filename = it
+                            showFileNameMissingError = filename.isBlank()
+                        },
+                        isError = hasInteractedWithFileName && showFileNameMissingError,
+                        modifier = Modifier.onFocusChanged {
+                            showFileNameMissingError = filename.isBlank()
+                            if (it.isFocused) {
+                                hasInteractedWithFileName = true
+                            } else {
+                                showFileNameMissingError = filename.isBlank()
+                            }
+                        },
                         label = { Text("Filename") },
                         singleLine = true
                     )
@@ -40,15 +65,17 @@ object AddFileDialog {
             },
             confirmButton =
                 {
-                    Button(onClick = {
-                        val separator =
-                            if (location[0] == '/') '/' else '\\' // OS using forward slashes have the root /
-                        supervisorIoScope.launch {
-                            addFile("$location$separator$filename")
-                            onConfirmed()
+                    Button(
+                        enabled = filename.isNotBlank(),
+                        onClick = {
+                            val separator =
+                                if (location[0] == '/') '/' else '\\' // OS using forward slashes have the root /
+                            supervisorIoScope.launch {
+                                addFile("$location$separator$filename")
+                                onConfirmed()
+                            }
+                            isShown = false
                         }
-                        isShown = false
-                    }
                     ) {
                         Text("OK")
                     }
